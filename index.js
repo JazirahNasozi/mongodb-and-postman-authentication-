346838dcae42bbbda9774464e169726f21fef1dc
JazirahNasozi <ellyjazmine@gmail.com>
2025-10-04 16:04:08 +0300

    post api

--- index.js
+++ index.js
@@ -4,19 +4,26 @@ require('dotenv').config();
 
 const userRoutes = require("./routes/users");
 
+const postRoutes = require("./routes/post");
+
 const app = express();
+
+// --- Body parser middleware ---
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send('Welcome to my posts api!');
 });
 
-//routes
-app.use('/user',userRoutes)
+// Routes
+app.use('/user', userRoutes);
+app.use('/post', postRoutes);
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-    
 
+// Connect to MongoDB
 connectDB();
---
+++ middleware/auth.js
@@ -0,0 +1,25 @@
+const e = require("express");
+const jwt = require("jsonwebtoken")
+
+
+const auth = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+
+        let decodedData;
+        if (token) {
+            decodedData = jwt.verify(token, "test");
+
+            req.userId = decodedData?.id;
+            console.log(decodedData);
+
+        }
+        next();
+
+    } catch (error) {
+        res.status(401).json({ message: "Invalid token" })
+
+    }
+}
+
+module.exports = auth;
