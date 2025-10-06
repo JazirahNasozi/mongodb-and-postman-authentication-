4daec57225ce977d58c1fd63be1f3fc751a266f5
JazirahNasozi <ellyjazmine@gmail.com>
2025-10-06 12:02:38 +0300

    posts api

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const connectDB = require('./db');
 require('dotenv').config();
 
 const userRoutes = require("./routes/users");
-
 const postRoutes = require("./routes/post");
 
 const app = express();
@@ -12,18 +11,18 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Connect to MongoDB first
+connectDB();
+
+// Routes
 app.get('/', (req, res) => {
     res.send('Welcome to my posts api!');
 });
-
-// Routes
 app.use('/user', userRoutes);
 app.use('/post', postRoutes);
 
+// Start server on the correct Render port
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`✅ Server is running on port ${PORT}`);
 });
-
-// Connect to MongoDB
-connectDB();
